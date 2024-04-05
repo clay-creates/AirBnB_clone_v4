544906be14fcc1bc04317bda481af686239e8400
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-04-04 20:04:45 -0500

    Adding files for #2, reorganizing/adding folders, and adding files for remaining questions

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(function () {
+  var amenities = {};
+  let ameniID = [];
+  $('input:checkbox').change(function () {
+    let ameniName = [];
+    $(':checkbox').each(function () {
+      if ($(this).is(":checked")) {
+        ameniID.push($(this).data('id'));
+        ameniName.push($(this).data('name'));
+      } else {
+        delete amenities[$(this).data('id')];
+      }
+    });
+    $('div.amenities h4').text(ameniName.join(', '));
+    console.log(ameniID);
+  });
+});
