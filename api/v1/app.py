fdd58e85c3ff7aa59133fe869571f948fb1dd2a9
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-04-07 21:32:54 -0500

    reverting

--- api/v1/app.py
+++ api/v1/app.py
@@ -48,6 +48,4 @@ if __name__ == "__main__":
         host = '0.0.0.0'
     if not port:
         port = '5000'
-    else:
-        port = '5001'
-    app.run(host=host, port=int(port), threaded=True)
+    app.run(host=host, port=port, threaded=True)
